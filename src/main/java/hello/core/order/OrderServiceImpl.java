33e5020f3ffdc9443b0a588d085ead1c72a254df
YunHyeRim1 <0305_08@naver.com>
2023-08-29 23:43:03 +0900

    관심사의 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,8 +9,8 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy; // 인터페이스에만 의존
+    private final MemberRepository memberRepository; // final로 되어 있으면 기본으로 할당을 하든 생성자를 통해서 할당되어야 한다.
+    private final DiscountPolicy discountPolicy; // 인터페이스에만 의존
     /*
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     추상(인터페이스) DiscountPolicy에 의존하고, 구체(구현)클래스 FixDiscountPolicy에도 의존한다. > DIP 위반!
@@ -18,6 +18,11 @@ public class OrderServiceImpl implements OrderService {
 
     FixDiscountPolicy > RateDiscountPolicy 변경하는 것은 OCP 위반!
      */
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
