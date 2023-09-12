7bffcf20a80f76919377eb14382ebb285d8e8eae
YunHyeRim1 <0305_08@naver.com>
2023-09-12 22:56:56 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -45,5 +46,23 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
 
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean(MemberService.class);
+        MemberService memberService2 = ac.getBean(MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
